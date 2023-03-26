2186658b9d4f34fd20740573173a0d7560165e38
alejandra <paulanaranjo50hotmail.com>
2023-03-26 18:49:40 -0500

    clear |enter the commit message for your changes. Lines starting

---
+++ JS-Orientado_Objetos/index.js
@@ -0,0 +1,25 @@
+class cliente
+{
+    nombreCliente;
+    cedulaCiudadania;
+    numeroCuenta;
+    saldoCuenta;
+}
+
+const cliente1 = new cliente();
+cliente1.nombreCliente = Sebastian;
+cliente1.cedulaCiudadania = 1920192830;
+cliente1.numeroCuenta = 000000101010;
+cliente1.saldoCuenta = 2000;
+
+const cliente2 = new cliente();
+cliente2.nombreCliente ="Alejandra";
+cliente2.cedulaCiudadania = "1020805061";
+cliente2.numeroCuenta = "3147824111"
+cliente2.saldoCuenta = 20000;
+
+
+console.log(cliente1);
+console.log(cliente2);
+
+
